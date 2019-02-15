c620f414024baf264db2e82f0754b8b7c846ad9a
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-15 22:44:06 +0000

    Create new Library and new Playlist. Add newPL to Lib.

--- Library.js
+++ Library.js
@@ -3,6 +3,10 @@ function Library (name, creator) {
   this.creator = creator;
 
   this.playlists = [];
+
+  this.addPL = function(newPL) {
+    this.playlists.push(newPL)
+  }
 }
 
 function Playlist (name) {
@@ -10,17 +14,22 @@ function Playlist (name) {
 
   this.tracks = [];
 
-  overallRating(this.tracks) {
-    // return sum of track ratings / num of tracks
-  }
-  totalDuration(this.tracks) {
-    // return sum of track lengths
-  }
+  // overallRating(this.tracks) {
+  //   // return sum of track ratings / num of tracks
+  // }
+  // totalDuration(this.tracks) {
+  //   // return sum of track lengths
+  // }
 }
 
 function Track (title, rating, length) {
   this.title = title;
   this.rating = rating;
   this.length = length;
-
 }
+
+const shmoosLibrary = new Library('dark80s', 'Shmoo');
+const siouxsiePL = new Playlist('Siouxsie');
+shmoosLibrary.addPL(siouxsiePL);
+
+
